b2175f5b36455fd9f086876be0356a9812229907
HughesSeanKyle <khughessean@yahoo.com>
2022-12-04 14:54:49 +0200

    Move persistence logic to services

---
+++ services/auth.js
@@ -0,0 +1,61 @@
+import User from '../models/User.js';
+
+/* CREATE NEW USER (CREATE)*/
+export const createUser = async ({
+	firstName,
+	lastName,
+	email,
+	passwordHash,
+	picturePath,
+	friends,
+	location,
+	occupation,
+}) => {
+	try {
+		const newUser = new User({
+			firstName,
+			lastName,
+			email,
+			password: passwordHash,
+			picturePath,
+			friends,
+			location,
+			occupation,
+			// Generate random num from 0 to 10000
+			viewedProfile: Math.floor(Math.random() * 10000),
+			impressions: Math.floor(Math.random() * 10000),
+		});
+		const savedUser = await newUser.save();
+
+		if (savedUser) {
+			return {
+				data: savedUser,
+				error: null,
+			};
+		}
+	} catch (error) {
+		return {
+			data: null,
+			error: error,
+		};
+	}
+};
+
+/* FIND EXISTING USER (READ)*/
+export const getUser = async (email) => {
+	try {
+		const user = await User.findOne({ email: email });
+
+		if (user) {
+			return {
+				data: user,
+				error: null,
+			};
+		}
+	} catch (error) {
+		return {
+			data: null,
+			error: error,
+		};
+	}
+};
